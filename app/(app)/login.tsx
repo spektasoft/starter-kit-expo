e2ba57143ae52511db82e5e58f6021dd853fc1eb
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-10-28 17:49:10 +0700

    refactor: remove redundant drawer screen component

--- app/(app)/login.tsx
+++ app/(app)/login.tsx
@@ -1,6 +1,5 @@
 import { Authenticated } from '@refinedev/core';
 import { Redirect } from 'expo-router';
-import Drawer from 'expo-router/drawer';
 import { ScrollView } from 'react-native';
 
 import { Container } from '~/components/Container';
@@ -10,7 +9,6 @@ import { AuthPage } from '~/components/pages/auth/AuthPage';
 export default function Login() {
   return (
     <>
-      <Drawer.Screen options={{ title: 'Login' }} />
       <ScrollView>
         <Container>
           <Authenticated
