fff4be8d56a97a95afc89171a8ed753ba14f5585
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-11-03 17:40:42 +0700

    feat: update user to error early

--- providers/auth-provider/user.ts
+++ providers/auth-provider/user.ts
@@ -1,9 +1,19 @@
+import { getItemAsync } from 'expo-secure-store';
 import { Platform } from 'react-native';
 
+import { getTokenKey } from '~/config';
+import { UnauthenticatedError } from '~/errors/UnauthenticatedError';
 import { getHttp } from '~/lib/http';
 import { User } from '~/models/User';
 
 export const user = async (): Promise<User> => {
+  if (Platform.OS !== 'web') {
+    const token = await getItemAsync(getTokenKey());
+    if (!token) {
+      throw new UnauthenticatedError();
+    }
+  }
+
   const http = await getHttp();
 
   const route = Platform.OS === 'web' ? 'user' : 'api/v1/user';
